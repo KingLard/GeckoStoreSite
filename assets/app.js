7b662c8d0502990703c1eaebb17715f8290f7158
Brett Landes <brettl0407@gmail.com>
2020-04-06 22:27:09 -0400

    added a new background photo for geckos and fixed an alignment issue with the two navbars

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,21 @@
 $("#snake-btn").on("click", function() {
     event.preventDefault();
     $("#snake-btn").addClass("active");
-    $("#lizard-btn, #turtle-btn, #arach-btn").removeClass("active")
+    $("#lizard-btn").removeClass("active")
     $("#snake-sec").removeClass("hidden");
-    $("#lizard-sec, #turtle-sec, #arach-sec").addClass("hidden");
+    $("#lizard-sec").addClass("hidden");
+    $(".reptile-jumbo").removeClass("lizard-bg");
+    $(".reptile-jumbo").addClass("snake-bg");
 })
 
 $("#lizard-btn").on("click", function() {
     event.preventDefault();
     $("#lizard-btn").addClass("active");
-    $("#snake-btn, #turtle-btn, #arach-btn").removeClass("active")
+    $("#snake-btn").removeClass("active")
     $("#lizard-sec").removeClass("hidden");
-    $("#snake-sec, #turtle-sec, #arach-sec").addClass("hidden");
+    $("#snake-sec").addClass("hidden");
+    $(".reptile-jumbo").addClass("lizard-bg");
+    $(".reptile-jumbo").removeClass("snake-bg");
     
 
 })
